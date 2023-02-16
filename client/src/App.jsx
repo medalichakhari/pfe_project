ae5fd5dee57c361b4ba87351e99c440f182a3a0e
mohamedali69 <dalichakhari6801@gmail.com>
2023-02-16 22:47:52 +0100

    Add client side structure

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "./components/navbar/NavBar";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
