945704bda922a10ffa4fca02773c34adcd56b4d8
mohamedali69 <dalichakhari6801@gmail.com>
2023-02-17 20:05:05 +0100

    made some style changes

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"]
       },
-      colors: {
-        primary: "#468EF9",
-        secondary: "#0C66EE",
-        blue: "#2F7CF0",
-        black: "#222222",
-        gray: "#666666",
-        lightgray: "#DDDDDD",
-        green: "#28C165",
-        red: "#F4574D",
-      },
+      // colors: {
+      //   primary: "#468EF9",
+      //   secondary: "#0C66EE",
+      //   blue: "#2F7CF0",
+      //   black: "#222222",
+      //   gray: "#666666",
+      //   lightgray: "#DDDDDD",
+      //   green: "#28C165",
+      //   red: "#F4574D",
+      // },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
