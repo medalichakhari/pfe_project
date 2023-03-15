2dcf6bead9a3419d3a63bc2742f3ba847917bea4
mohamedali69 <dalichakhari6801@gmail.com>
2023-03-15 16:38:38 +0100

    fixing snackbar

--- client/src/components/shared/SnackBarAlert.jsx
+++ client/src/components/shared/SnackBarAlert.jsx
@@ -1,27 +1,29 @@
-// import React, { useState } from "react";
-// import Snackbar from "@mui/material/Snackbar";
-// import MuiAlert from "@mui/material/Alert";
+import React, { useState } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import { Alert } from "@mui/material/Alert";
 
-// const Alert = React.forwardRef(function Alert(props, ref) {
-//   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-// });
+const SnackBarAlert = ({ message, type }) => {
+  const [open, setOpen] = useState(true);
 
-// export default function SnackBarAlert({ message, type }) {
-//   const [open, setOpen] = useState(true);
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-//   const handleClose = (event, reason) => {
-//     if (reason === "clickaway") {
-//       return;
-//     }
+    setOpen(false);
+  };
 
-//     setOpen(false);
-//   };
+  return (
+    <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
+      <Alert
+        onClose={handleClose}
+        severity={type}
+        sx={{ width: "100%" }}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
 
-//   return (
-//     <Snackbar open={open} autoHideDuration={3000} onClose={handleClose}>
-//       <Alert onClose={handleClose} severity={type} sx={{ width: "100%" }}>
-//         {message}
-//       </Alert>
-//     </Snackbar>
-//   );
-// }
+export default SnackBarAlert;
