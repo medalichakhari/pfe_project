5c09943771f231f992f777b55d8ae4ecd320174a
aminbrahmi <aminebrahmity12@gmail.com>
2023-04-14 18:49:25 +0100

    xx

--- client/src/pages/jobapplication/JobApplication.jsx
+++ client/src/pages/jobapplication/JobApplication.jsx
@@ -2,7 +2,7 @@ import { useFormik } from "formik";
 import { useState } from "react";
 import Layout from "../../components/layout/Layout";
 import CandidatForm from "../../components/candidatform/CandidatForm";
-import PrimaryButton from "../../components/buttons/PrimaryButton";
+import PrimaryButton from "../../components/buttons/primarybutton";
 import SecondaryButton from "../../components/buttons/SecondaryButton";
 import { useAuth } from "../../context/AuthContext";
 import { CreateCandidat } from "../../lib/fetch";
@@ -39,7 +39,9 @@ const JobApplication = () => {
           <SecondaryButton type="button" onClick={previousFormStep}>
             Previous
           </SecondaryButton>
-          <PrimaryButton type="submit" onClick={completeFormStep}>Apply</PrimaryButton>
+          <PrimaryButton type="submit" onClick={completeFormStep}>
+            Apply
+          </PrimaryButton>
         </div>
       );
     } else {
@@ -53,51 +55,49 @@ const JobApplication = () => {
       grade: values.grade,
       speciality: values.speciality,
       cv: values.cv,
-      };
-      CreateCandidat(candidatData, token)
-        .then((res) => {
-          console.log(res);
-          navigate("/JobApplication");
-        })
-        .catch((err) => console.log(err));
     };
-  
-    const {
-      values: applyJobValues,
-      errors: applyJobErrors,
-      touched: applyJobTouched,
-      isSubmitting: applyJobIsSubmitting,
-      handleBlur: applyJobHandleBlur,
-      handleChange: applyJobHandleChange,
-      handleSubmit: applyJobHandleSubmit,
-    } = useFormik({
-      initialValues: {
-          speciality: "",
-          grade: "",
-        },
-        onSubmit: handleApplyJob,
-    });
+    CreateCandidat(candidatData, token)
+      .then((res) => {
+        console.log(res);
+        navigate("/JobApplication");
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const {
+    values: applyJobValues,
+    errors: applyJobErrors,
+    touched: applyJobTouched,
+    isSubmitting: applyJobIsSubmitting,
+    handleBlur: applyJobHandleBlur,
+    handleChange: applyJobHandleChange,
+    handleSubmit: applyJobHandleSubmit,
+  } = useFormik({
+    initialValues: {
+      speciality: "",
+      grade: "",
+    },
+    onSubmit: handleApplyJob,
+  });
   return (
     <Layout>
-<<<<<<< HEAD
       <div className="my-6 flex justify-center items-center bg-gray-50">
         <div className="w-full max-w-xl p-4 bg-white border border-gray-200 rounded-lg shadow-xl sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
           <div>
             {formStep === 0 && <CandidatInfo />}
-          {renderCandidatButtons()}
+            {renderCandidatButtons()}
           </div>
-          <div className="flex flex-row-reverse">
-        </div>
-        <form onSubmit={applyJobHandleSubmit}>
+          <div className="flex flex-row-reverse"></div>
+          <form onSubmit={applyJobHandleSubmit}>
             {formStep === 1 && (
               <CandidatForm
-              values={applyJobValues}
-              handleChange={applyJobHandleChange}
-              handleBlur={applyJobHandleBlur}
-            />
-          )}
-          
-          {renderApplyJobButtons()}
+                values={applyJobValues}
+                handleChange={applyJobHandleChange}
+                handleBlur={applyJobHandleBlur}
+              />
+            )}
+
+            {renderApplyJobButtons()}
           </form>
           {formStep === 2 && (
             <div className="mb-2">
@@ -107,9 +107,6 @@ const JobApplication = () => {
           )}
         </div>
       </div>
-=======
-      
->>>>>>> 63d0ef3668f1e818409694b11fb3c9433cd51321
     </Layout>
   );
 };
