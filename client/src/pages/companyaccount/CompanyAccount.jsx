9eb689ab42171e06caa0ba4fcb32186e3c41805b
mohamedali69 <dalichakhari6801@gmail.com>
2023-09-19 12:13:31 +0100

    delete bg gray

--- client/src/pages/companyaccount/CompanyAccount.jsx
+++ client/src/pages/companyaccount/CompanyAccount.jsx
@@ -57,7 +57,7 @@ const CompanyAccount = () => {
   });
   return (
     <Layout>
-      <div className="flex justify-center items-center bg-gray-50">
+      <div className="flex justify-center items-center">
         <div className="w-full max-w-xl p-4 bg-white border border-gray-200 rounded-lg shadow-xl sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
           <form onSubmit={handleSubmit}>
             <CompanyForm
--- client/src/pages/jobposting/JobPosting.jsx
+++ client/src/pages/jobposting/JobPosting.jsx
@@ -126,7 +126,7 @@ const JobPosting = () => {
   });
   return (
     <Layout>
-      <div className="flex justify-center items-center bg-gray-50">
+      <div className="flex justify-center items-center">
         <div className="w-full max-w-xl p-4 bg-white border border-gray-200 rounded-lg shadow-xl sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
           <div>
             {formStep === 0 && <CompanyInfo />}
